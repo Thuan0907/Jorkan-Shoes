d0bb4ce7c094a4e877631f2de2e9366109d2b90e
Thuan0907 <jinkin0019@gmail.com>
2022-08-19 20:09:30 +0700

    fix responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function getId(id) {
   return document.getElementById(id);
 }
@@ -6,9 +5,41 @@ function getId(id) {
 var navContent = getId("navContent");
 function showNavContent() {
   navContent.classList.add("active");
-  document.body.style.overflow='hidden'
+  document.body.style.overflow = "hidden";
 }
+
 function closePanel() {
-    navContent.classList.remove("active");
-    document.body.style.overflow='auto'
+  navContent.classList.remove("active");
+  document.body.style.overflow = "auto";
 }
+
+// window.onscroll = function () {
+//   myFunction();
+// };
+
+// var wrapNav = document.getElementsByClassName("wrapNav");
+// var sticky = wrapNav.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     wrapNav.classList.add("sticky");
+//   } else {
+//     wrapNav.classList.remove("sticky");
+//   }
+// }
+
+// var mybutton = document.getElementById("scrollTop");
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
